399b4e67d9ca07dbfc8e47473f6dbd2a5df0ac55
David Glaude <dglaude@users.noreply.github.com>
2023-07-09 00:49:04 +0200

    Update myconfig.py
    Comment out the default value from myconfig.py

--- myconfig.py
+++ myconfig.py
@@ -1,5 +1,5 @@
-RAINBOW = True           # Display a rainbow when active
-RAINBOW_DELAY = 50       # Mouse movement occurs every 256*RAINBOW_DELAY ticks, that mean every rainbow rotation reach red.
-AUTO_START = True        # Do you want jiggling to start automatically.
-WARN_NO_USB = True       # Do you want high speed blinking LED when USB is not connected.
-STEALTH_DEFAULT = False  # Do you want to hide the mass storage by default (short Gnd and 4 to reverse)
+#RAINBOW = True           # Display a rainbow when active
+#RAINBOW_DELAY = 50       # Mouse movement occurs every 256*RAINBOW_DELAY ticks, that mean every rainbow rotation reach red.
+#AUTO_START = True        # Do you want jiggling to start automatically.
+#WARN_NO_USB = True       # Do you want high speed blinking LED when USB is not connected.
+#STEALTH_DEFAULT = False  # Do you want to hide the mass storage by default (short Gnd and 4 to reverse)
